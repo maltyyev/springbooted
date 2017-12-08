30a62fa533ac2b73e8202b68b716f8bc55d07dc6
maltyyev <amk0794@gmail.com>
2017-12-08 19:09:42 +0500

    Init

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,76 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String ispn;
+    private String publisher;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {}
+
+    public Book(String title, String ispn, String publisher) {
+        this.title = title;
+        this.ispn = ispn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String ispn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.ispn = ispn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIspn() {
+        return ispn;
+    }
+
+    public void setIspn(String ispn) {
+        this.ispn = ispn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
