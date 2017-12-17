bc2b8395243056a53e874b58a329a7ff19c0d2b6
maltyyev <amk0794@gmail.com>
2017-12-17 23:21:41 +0500

    I don't remember what were the changes

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -10,6 +10,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -30,27 +33,42 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
 
+        List<Publisher> publishers = new ArrayList<>();
+        List<Author> authors = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+
         Publisher work = new Publisher("Work", "sacvetn");
         Publisher harperCollins = new Publisher("Harper Collins", "sdsafg");
-
-        publisherRepository.save(work);
-        publisherRepository.save(harperCollins);
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "1234", harperCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development Without EJB", "23444", work);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
+        Book book = new Book("Our book", "123456", harperCollins);
+        book.getAuthors().add(eric);
+        book.getAuthors().add(rod);
+        rod.getBooks().add(book);
+        eric.getBooks().add(book);
+
+        publishers.add(work);
+        publishers.add(harperCollins);
+
+        authors.add(eric);
+        authors.add(rod);
+
+        books.add(ddd);
+        books.add(noEJB);
+        books.add(book);
+
+        publisherRepository.saveAll(publishers);
+        authorRepository.saveAll(authors);
+        bookRepository.saveAll(books);
     }
 
 }
---
+++ src/main/java/guru/springframework/spring5webapp/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+
+
+}
